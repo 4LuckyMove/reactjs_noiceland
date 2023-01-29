8b8122366e5a7e64792e1f2ffc42ee78b2076927
Lucky4Move <linkoln2896@gmail.com>
2023-01-29 22:43:51 +0200

    heroku json server

--- src/API/baseURL.ts
+++ src/API/baseURL.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-	baseURL: 'http://localhost:3001/',
+	baseURL: 'https://json-server-noiceland.herokuapp.com/',
 	headers: { 'X-Custom-Header': 'noiceland' },
 })
